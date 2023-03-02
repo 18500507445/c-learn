bbb5504b61f56e122313e5ab9f30302035f99e11
wangzehui <18500507445@163.com>
2023-03-02 15:48:20 +0800

    优化结构

---
+++ main.c
@@ -0,0 +1,14 @@
+//引入标准库头文件
+#include <stdio.h>
+
+/**
+ * 主函数，程序入口
+ * @return
+ */
+int main() {
+    printf("Hello, World!\n");
+
+    //可以忽略不写，编译器会自动添加
+    return 0;
+}
+
--- number.c
+++ number.c
@@ -1,12 +1,6 @@
-//引入标准库头文件
 #include <stdio.h>
 
-/**
- * 主函数，程序入口
- * @return
- */
 int main() {
-    printf("Hello, World!\n");
 
     int a = 5, b = 10;
     int c = a + b;
@@ -17,8 +11,5 @@ int main() {
 
     char f = 65;
     printf("d = %c\n", f);
-
-    //可以忽略不写，编译器会自动添加
-    return 0;
 }
 
